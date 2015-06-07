70f5121971371822274cd0d031a254de991e61d2
zagagenia <genius43@hotmail.comgit config --global user.email genius43@hotmail.com>
2015-06-08 00:15:54 +0200

    Initial commit

---
+++ plot2.R
@@ -0,0 +1,21 @@
+setwd("C:/Users/user/Desktop/Coursera/EDA")
+
+## Getting the dataset
+
+data_full <-read.csv("C:/Users/user/Desktop/Coursera/EDA/exdata-data-household_power_consumption/household_power_consumption.txt", header=T, sep=';', na.strings="?", nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+data_full$Date <- as.Date(data_full$Date, format="%d/%m/%Y")
+
+
+## Subsetting the data
+data <- subset(data_full, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(data_full)
+
+## Converting dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Plot 2
+plot(data$Global_active_power~data$Datetime, type="l",
+     ylab="Global Active Power (kilowatts)", xlab="")
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
